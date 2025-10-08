eb12444b15779c65e24ed728659555e87f605ca9
yugyeong71 <youkyoung42@gmail.com>
2025-10-08 14:11:29 +0900

    fix : Code 명명 규칙 수정

--- src/main/kotlin/com/meow/meowchatting/common/exception/MeowCode.java
+++ src/main/kotlin/com/meow/meowchatting/common/exception/MeowCode.java
@@ -4,8 +4,8 @@ import org.springframework.http.HttpStatus;
 
 /**
  * [Meow Code 명명 규칙]
- * - 클래스 : Meow{도메인명}Code
- * - Code : MEOW_{도메인명}_{RESPONSE CODE 명칭}
+ * - 클래스 : {도메인명}ResponseCode
+ * - Code : {코드로 표현하고 싶은 상태}
  */
 public interface MeowCode {
 
--- src/main/kotlin/com/meow/meowchatting/user/domain/error/UserResponseCode.java
+++ src/main/kotlin/com/meow/meowchatting/user/domain/error/UserResponseCode.java
@@ -7,9 +7,9 @@ import com.meow.meowchatting.common.exception.MeowCode;
 import lombok.Getter;
 
 @Getter
-public enum MeowUserCode implements MeowCode {
+public enum UserResponseCode implements MeowCode {
 
-	MEOW_USER_NOT_FOUND(1000, "사용자를 찾을 수 없습니다.", HttpStatus.NOT_FOUND); // TODO : 도메인 별 responseCode 범위 설정하기
+	NOT_FOUND(1000, "사용자를 찾을 수 없습니다.", HttpStatus.NOT_FOUND); // TODO : 도메인 별 responseCode 범위 설정하기
 
 	private final int responseCode;
 
@@ -17,7 +17,7 @@ public enum MeowUserCode implements MeowCode {
 
 	private final HttpStatus httpStatus;
 
-	MeowUserCode(int responseCode, String responseMessage, HttpStatus httpStatus) {
+	UserResponseCode(int responseCode, String responseMessage, HttpStatus httpStatus) {
 		this.responseCode = responseCode;
 		this.responseMessage = responseMessage;
 		this.httpStatus = httpStatus;
